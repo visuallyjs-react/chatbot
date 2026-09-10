462f3cd1f9652132033c97984f7d57bf4d4cfe58
Simon Porritt <simon@jsplumbtoolkit.com>
2026-09-10 17:44:15 +1000

    rename original dataset and switch main to use 'dataset'

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.querySelector("#root")).render(
   <StrictMode>
-    <App url="/helpdesk.json"/>
+    <App url="/dataset.json"/>
   </StrictMode>,
 )
